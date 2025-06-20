6d03f7de0242ebd12aa5b8adec4966f0198c42d9
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-06-20 20:06:00 +0600

    completed: roleMiddleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,34 @@
 const express = require("express");
 
+const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
+
 const router = express.Router();
 
 // Admin routes
-router.get("/admin", (req, res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
     res.status(200).json({ message: "Admin route accessed" });
 });
 
 // Manager routes
-router.get("/manager", (req, res) => {
-    res.status(200).json({ message: "Manager route accessed" });
-});
+router.get(
+    "/manager",
+    verifyToken,
+    authorizeRoles("admin", "manager"),
+    (req, res) => {
+        res.status(200).json({ message: "Manager route accessed" });
+    }
+);
 
 // User routes
-router.get("/user", (req, res) => {
-    res.status(200).json({ message: "User route accessed" });
-});
+router.get(
+    "/user",
+    verifyToken,
+    authorizeRoles("admin", "manager", "user"),
+    (req, res) => {
+        res.status(200).json({ message: "User route accessed" });
+    }
+);
 
 module.exports = router;
 // This file defines the user routes for different roles
