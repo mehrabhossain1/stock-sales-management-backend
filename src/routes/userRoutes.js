e77d29ca8c11200834667d5700aa6e7d50013619
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-06-29 20:14:18 +0600

    feat: user/me route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const verifyToken = require("../middlewares/authMiddleware");
 const authorizeRoles = require("../middlewares/roleMiddleware");
+const userModel = require("../models/userModel");
 
 const router = express.Router();
+
+router.get("/me", verifyToken, async (req, res) => {
+    try {
+        const user = await userModel.findById(req.user.id).select("-password");
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        res.status(200).json({ user });
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch profile" });
+    }
+});
 
 // Admin routes
 router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
